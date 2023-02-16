c330fa57407dd91d15dd4d73ded30d44cfe80d35
yasmineahmed11 <yasmineahmedsayed33@gmail.com>
2023-02-16 02:48:20 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
- * * main -Entry
- * * Return: Always 0
- * */
+ * main -Entry
+ * Return: Always 0
+ */
+
 int main(void)
 {
  puts("\"Programming is like a multilingual puzzle");
