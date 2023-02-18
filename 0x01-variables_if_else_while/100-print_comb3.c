7b5d449dc6d7262ec9f6a8b84dcacc581c21ce78
yasmineahmed11 <yasmineahmedsayed33@gmail.com>
2023-02-18 02:21:54 -0800

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,23 @@
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i + '0');
+		for (j = 1 ; j < 10 ; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
 	}
 putchar('\n');
 return (0);
