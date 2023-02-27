48353c3c6281be343d1bd774d3f266e6ced01dca
yasmineahmed11 <yasmineahmedsayed33@gmail.com>
2023-02-27 08:47:55 -0800

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - given two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
