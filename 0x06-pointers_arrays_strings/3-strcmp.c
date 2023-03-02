1591c8e027bd95f0c4ff3fd7ef8ee80106993ee2
yasmineahmed11 <yasmineahmedsayed33@gmail.com>
2023-03-02 02:21:28 -0800

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcmp - compare strings
+ * @s1: strings
+ * @s2: strings
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
